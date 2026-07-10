381e5f8fda54a23b1f806bdede273a9abfd19b9a
Yash Chaurasia <peterchaurasia@gmail.com>
2026-07-10 14:36:30 +0530

    fix backend

--- backend/app.py
+++ backend/app.py
@@ -13,11 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://you-tube-rag-ai.vercel.app"
-    ],
-    allow_origin_regex="https://.*vercel.app",
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
